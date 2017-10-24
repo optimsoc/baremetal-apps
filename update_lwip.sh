aa359b89100ca81d05272fbdc63ba5c467491ce8
Annika Fuchs <annika.fuchs@tum.de>
2017-10-24 14:20:06 +0200

    Update shell script for LwIP

--- update_lwip.sh
+++ update_lwip.sh
@@ -11,7 +11,7 @@ WD_HAS_CHANGES=$(git diff-index --quiet HEAD --; echo $?)
 [ $WD_HAS_CHANGES == 1 ] && git stash
 
 # lwip
-git subtree pull -m "Update lwip_demo" --prefix lwip_demo/lwip https://git.savannah.gnu.org/git/lwip.git master --squash
+git subtree pull -m "Update lwip stack in lwip_demo/lwip" --prefix lwip_demo/lwip https://git.savannah.gnu.org/git/lwip.git master --squash
 
 # reapply our changes to the working directory
 [ $WD_HAS_CHANGES == 1 ] && git stash pop
